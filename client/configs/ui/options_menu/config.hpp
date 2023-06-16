ac80e73114708cf7e7ba0151b5e35c855c4e0638
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-06-16 10:52:24 +0200

    Add para_disablePhotoCameraSaving to Option Menu (#4)
    * Add para_disablePhotoCameraSaving to Option Menu
    
    * Unify indentation to TABs

--- client/configs/ui/options_menu/config.hpp
+++ client/configs/ui/options_menu/config.hpp
@@ -1,99 +1,111 @@
 class para_Option {
-    name = "Option Name"; // Display Name
-    variable = "my_option_name"; // Variable name for saving in PFS
-    tooltip = ""; // Tooltip
+	name = "Option Name"; // Display Name
+	variable = "my_option_name"; // Variable name for saving in PFS
+	tooltip = ""; // Tooltip
 };
 
 class para_OptionCheckbox: para_Option {
-    type = "Checkbox";
-    default = 0;
+	type = "Checkbox";
+	default = 0;
 };
 
 class para_OptionSlider: para_Option {
-    type = "Slider";
-    default = 50;
-    range[] =  { 0, 100 };
-    step = 10;
+	type = "Slider";
+	default = 50;
+	range[] =  { 0, 100 };
+	step = 10;
 };
 
 class para_OptionCombobox: para_Option {
-    type = "Combobox";
-    default = 0;
-    values[] = {
-        "Value Name",
-        "Value Name 2"
-    };
+	type = "Combobox";
+	default = 0;
+	values[] = {
+		"Value Name",
+		"Value Name 2"
+	};
 };
 
 class para_CfgOptions {
-    class para_enableWelcomeScreen {
-        name = "Enable welcome screen";
-        tooltip = "Enables the welcome screen on mission start";
-        type = "Checkbox";
-        default = 0;
-    };
-    class para_enableTutorial {
-        name = "Enable gamemode tutorials";
-        tooltip = "Enables tutorial card popups with gameplay hints.";
-        type = "Checkbox";
-        default = 1;
-    };
-    class para_enableDynamicViewDist {
-        name = "Enable dynamic view distance settings";
-        tooltip = "Enables all dynamic view distance settings, only disable it if you are using your own view distance mods";
-        onChange = "para_c_perf_enable_dynamic_view_distance = _newValue";
-        type = "Checkbox";
-        default = 1;
-    };
-    class para_minViewDist {
+	class para_enableWelcomeScreen {
+		name = "Enable welcome screen";
+		tooltip = "Enables the welcome screen on mission start";
+		type = "Checkbox";
+		default = 0;
+	};
+	class para_enableTutorial {
+		name = "Enable gamemode tutorials";
+		tooltip = "Enables tutorial card popups with gameplay hints.";
+		type = "Checkbox";
+		default = 1;
+	};
+	class para_enableDynamicViewDist {
+		name = "Enable dynamic view distance settings";
+		tooltip = "Enables all dynamic view distance settings, only disable it if you are using your own view distance mods";
+		onChange = "para_c_perf_enable_dynamic_view_distance = _newValue";
+		type = "Checkbox";
+		default = 1;
+	};
+	class para_minViewDist {
 		name = "Min. view distance"; // Display Name
 		tooltip = "Minimum distance that the view distance will be reduced to"; // Tooltip
-        onChange = "para_c_perf_min_view_distance = _newValue";
+		onChange = "para_c_perf_min_view_distance = _newValue";
 		type = "Slider";
 		default = 800;
 		range[] =  { 800, 12000 };
 		step = 100;
 	};
-    class para_minObjectViewDist {
+	class para_minObjectViewDist {
 		name = "Min. object view distance";
 		tooltip = "Minimum distance that object rendering will be reduced to";
-        onChange = "para_c_perf_min_object_view_distance = _newValue";
+		onChange = "para_c_perf_min_object_view_distance = _newValue";
 		type = "Slider";
 		default = 800;
 		range[] =  { 800, 12000 };
 		step = 100;
 	};
-    class para_maxViewDist {
+	class para_maxViewDist {
 		name = "Max. view distance"; // Display Name
 		tooltip = "Maximum distance that the view distance will be increased to"; // Tooltip
-        onChange = "para_c_perf_max_view_distance = _newValue";
+		onChange = "para_c_perf_max_view_distance = _newValue";
 		type = "Slider";
 		default = 2000;
 		range[] =  { 800, 12000 };
 		step = 100;
 	};
-    class para_maxObjectViewDist {
+	class para_maxObjectViewDist {
 		name = "Max. object view distance";
 		tooltip = "Maximum distance that object rendering will be increased to";
-        onChange = "para_c_perf_max_object_view_distance = _newValue";
+		onChange = "para_c_perf_max_object_view_distance = _newValue";
 		type = "Slider";
 		default = 2000;
 		range[] =  { 800, 12000 };
 		step = 100;
 	};
-    class para_minFpsViewDist {
-        name = "Min. desired FPS";
+	class para_minFpsViewDist {
+		name = "Min. desired FPS";
 		tooltip = "Lower FPS than this will cause view distance to start decreasing";
-        onChange = "para_c_perf_min_fps_to_reduce_view_distance = _newValue";
+		onChange = "para_c_perf_min_fps_to_reduce_view_distance = _newValue";
 		type = "Slider";
 		default = 20;
 		range[] =  { 0, 120 };
 		step = 10;
 	};
-    class para_disableDynViewDistWhenFlying {
-        name = "Disable dynamic view distance when flying";
+	class para_disableDynViewDistWhenFlying {
+		name = "Disable dynamic view distance when flying";
 		tooltip = "Forces the view distance to the max allowed when flying";
-        onChange = "para_c_perf_disable_dynamic_view_distance_when_flying = _newValue";
+		onChange = "para_c_perf_disable_dynamic_view_distance_when_flying = _newValue";
+		type = "Checkbox";
+		default = 0;
+	};
+	class para_disablePhotoCameraSaving {
+		name = "Disable Photo Camera files saving";
+		tooltip = "Prevents photo camera photos from being saved to disk";
+		onChange = "\
+			!_newValue spawn {\
+				waitUntil {missionNamespace getVariable ['vn_photoCamera_initialized', false]};\
+				vn_photoCamera_allowed = _this;\
+			};\
+		";
 		type = "Checkbox";
 		default = 0;
 	};
